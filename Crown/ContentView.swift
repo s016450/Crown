3b4cb65d328e5249e1b21e9496be8443fc068a25
Jonathan Miller <s016450@students.lmsd.org>
2022-02-16 10:53:42 -0500

    Initial Commit

---
+++ Crown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crown
+//
+//  Created by Jonathan Miller (student LM) on 2/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crown/CrownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrownApp.swift
+//  Crown
+//
+//  Created by Jonathan Miller (student LM) on 2/16/22.
+//
+
+import SwiftUI
+
+@main
+struct CrownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
