e55f929c6592695ef353657076a0a2a6bb6533ac
Maya Ringold <s015394@students.lmsd.org>
2022-02-23 08:53:59 -0500

    Comp List View

---
+++ Crown/CompListView.swift
@@ -0,0 +1,20 @@
+//
+//  CompListView.swift
+//  Crown
+//
+//  Created by Maya Ringold (student LM) on 2/23/22.
+//
+
+import SwiftUI
+
+struct CompListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompListView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompListView()
+    }
+}
