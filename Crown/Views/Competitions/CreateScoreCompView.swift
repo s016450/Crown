fc28d8c66f6ee5a5878158f82ad157955689d633
Sophia Weatherwax <s015406@students.lmsd.org>
2022-03-02 10:27:27 -0500

    nav view

--- Crown/Views/Competitions/CreateScoreCompView.swift
+++ Crown/Views/Competitions/CreateScoreCompView.swift
@@ -12,53 +12,50 @@ struct CreateScoreCompView: View {
     @EnvironmentObject var competitionInfo: CompetitionInfo
     
     var body: some View { 
-          
-        VStack{
-            Text("New Score Competition")
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(Color.Gray)
-            
-            //enter competition name
-            TextField("Competition Name", text: $competitionInfo.compName)
-                .frame(maxWidth: .infinity,
-                       minHeight: 44)
-                .padding(.leading, 30)
-                .keyboardType(.emailAddress)
-                .disableAutocorrection(true)
-                .autocapitalization(.none)
-                .foregroundColor(Color.Gray.opacity(0.5))
-                .background(
-                    
-                    ZStack(alignment: .leading){
-                        
-                        
-                        Image(systemName: "envelope")
-                            .font(.system(size: 16, weight: .semibold))
-                            .padding(.leading, 5)
-                            .foregroundColor(Color.Gray.opacity(0.5))
-                        
-                        RoundedRectangle(cornerRadius: 10, style: .continuous)
-                            .stroke(Color.Gray.opacity(0.5))
-                    }
-                )
-            
-            
-            //user info
-            //array to add users
-            Text("Enter Competitors Below")
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(Color.Gray)
-            
-              
-            
-            //input number of points needed
-            
-            Text("Enter Points End Goal")
-                .font(.system(size: 24, weight: .bold))
-                .foregroundColor(Color.Gray)
-     
-        }//end vstack
         
+        NavigationView{
+            
+            VStack{
+                Text("New Score Competition")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color.Gray)
+                
+                //enter competition name
+                TextField("Competition Name", text: $competitionInfo.compName)
+                    .frame(maxWidth: .infinity,
+                           minHeight: 44)
+                    .padding(.leading, 30)
+                    .keyboardType(.emailAddress)
+                    .disableAutocorrection(true)
+                    .autocapitalization(.none)
+                    .foregroundColor(Color.Gray.opacity(0.5))
+                
+                
+                
+                //user info
+                //array to add users
+                Text("Enter Competitors Below")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color.Gray)
+                
+                
+                
+                //input number of points needed
+                
+                Text("Enter Points End Goal")
+                    .font(.system(size: 24, weight: .bold))
+                    .foregroundColor(Color.Gray)
+                
+            }//end vstack
+            
+            
+            NavigationLink(
+                destination: ScoreCompView(),
+                label: {
+                    Text("Create Competetion")
+                })
+            
+        }//end nav view
         
     }
 }
