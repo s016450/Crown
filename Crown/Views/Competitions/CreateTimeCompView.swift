14d90773838357e002a0c58049c249f9018f44f6
Maya Ringold <s015394@students.lmsd.org>
2022-03-08 10:31:29 -0500

    time comp create view

--- Crown/Views/Competitions/CreateTimeCompView.swift
+++ Crown/Views/Competitions/CreateTimeCompView.swift
@@ -15,50 +15,62 @@ struct CreateTimeCompView: View {
     @StateObject var competitionInfo : CompetitionInfo = CompetitionInfo()
     
     //@StateObject var userInfo : UserInfo() = UserInfo()
-
+    
+    //date picker stuff https://stackoverflow.com/questions/52984284/swift-save-uidatepicker-date-into-coredata
+    
     var body: some View {
         NavigationView{
             VStack{
                 Text("New Time Competition")
-                    .font(.system(size: 40, weight: .bold))
-                    .foregroundColor(Color.Purple)
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundColor(Color.Yellow)
+                    .background(Color.Purple)
+                    .padding(.top, 100)
+                    .cornerRadius(10.0)
                 
-                TextField("Competition Name", text:  $competitionInfo.compName)
+                
+                TextField("Enter Competition Name", text:  $competitionInfo.compName)
                     .frame(maxWidth: .infinity, minHeight: 44)
-                    .padding(.leading, 30)
+                    .padding(.top, 40)
+                    .padding(.leading , 10)
                     .font(.system(size: 20, weight: .bold))
                     .foregroundColor(Color.Purple)
-                                
+                
                 //Users Info
                 Text("Enter The Competitors")
-                    .font(.system(size: 18, weight: .bold))
+                    .font(.system(size: 20, weight: .bold))
                     .foregroundColor(Color.Purple)
+                    .padding()
                 //use an array to add users
-                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+                Spacer(minLength: 0)
                 
                 //Time Stuff like
                 //(calender/days)
                 Text("Enter Duration of Competition")
-                    .font(.system(size: 18, weight: .bold))
-                    .foregroundColor(Color.Gray)
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color.Purple)
                 
-                DatePicker("End Date", selection: $enddate)
-                    .foregroundColor(.Purple)
+                DatePicker("Select End Date", selection: $enddate)
+                    .foregroundColor(Color.Purple)
+                    .padding()
+                
                 
                 //Button that takes you to the time competition
                 //Navigation View
                 Spacer(minLength: 0)
+                
                 NavigationLink(
                     destination: TimeCompView(),
                     label: {
                         Text("Create Competetion")
-                            .font(.system(size: 20, weight: .regular))
+                            .font(.system(size: 40, weight: .regular))
                             .foregroundColor(.Purple)
-                            .background(Color.Yellow)
-                            .cornerRadius(15.0)
-                    })
+                            .border(Color.Yellow, width: 2)
+                       
+                        
+                    }).padding()
                 Spacer(minLength: 0)
-            }
+            }.ignoresSafeArea(.all)
             
             
             
--- Crown/Views/Competitions/TimeCompView.swift
+++ Crown/Views/Competitions/TimeCompView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct TimeCompView: View {
+    
     var body: some View {
         Text("Time Competition")
     }
