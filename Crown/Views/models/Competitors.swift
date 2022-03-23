455ecbe2c1c5afa0edace044b19a551803ba1628
Sophia Weatherwax <s015406@students.lmsd.org>
2022-03-23 08:23:14 -0400

    competitor

--- Crown/Views/models/Competitors.swift
+++ Crown/Views/models/Competitors.swift
@@ -11,12 +11,26 @@ struct Competitors : Identifiable{
     var user : String
     var points : Int
     var id = UUID()
-  
+    
     
     init(user : String = "User", points : Int ){
         self.user = user
         self.points = points
         
     }
+    
+    func dataAsDictionaryCompetitor() -> [String: Any]{
+        
+        var data: [String: Any] = [String: Any]()
+        
+        data["user"] = user
+        data["points"] = points
+        data["id"] = id.uuidString
+        
+        return data
+        
+    }//end function
+    
+    
     
 }
